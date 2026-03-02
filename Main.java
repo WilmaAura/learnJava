34b0f885c3d65bb6e0a12fd322317b5989611a65
wilma <mornlitoralis@gmail.com>
2026-03-02 09:57:59 +0700

    some changes

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) /* args stands for arguments. its for array of strings */ 
+    throws Exception {
+        String name = "Wilma";
+        System.out.println("hello " + name);
+        System.out.println("RORRRR");
+    }
+}
