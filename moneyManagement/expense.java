95053180133e5adabcaad3bb000eaadb75f8094c
wilma <mornlitoralis@gmail.com>
2026-03-04 08:49:22 +0700

    some changes on money management

--- moneyManagement/expense.java
+++ moneyManagement/expense.java
@@ -2,30 +2,32 @@ package moneyManagement;
 
 import java.time.LocalDate;
 
-public class expense {
+class Expense {
     private int id;
     private String nBarang;
-    private String jBarang;
+    private String jBarang; // Primer, Sekunder, Tersier
     private double amount;
     private LocalDate tgl;
 
-    public Expense(int id, String nbarang, String jBarang,  double amount, LocalDate tgl){
+    public Expense(int id, String nBarang, String jBarang, double amount, LocalDate tgl) {
         this.id = id;
         this.nBarang = nBarang;
         this.jBarang = jBarang;
         this.amount = amount;
         this.tgl = tgl;
     }
-public double getAsmount(){
-    return amount;
-}
-public LocalDate getTgl(){
-    return tgl;
-}
-public String toString (){
-    return id + " | " + nBarang + " | " + jBarang + " | " + amount + " | " + tgl;
-}
 
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDate gettgl() {
+        return tgl;
+    }
+
+    public String toString() {
+        return id + " | " + nBarang + " | " + jBarang + " | " + amount + " | " + tgl;
+    }
 }
 
 
