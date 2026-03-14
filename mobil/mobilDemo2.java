7306cd5185f19641b301a2eec37245befb41e684
wilma <mornlitoralis@gmail.com>
2026-03-14 16:37:43 +0700

    some changes

---
+++ mobil/mobilDemo2.java
@@ -0,0 +1,20 @@
+package mobil;
+
+
+public class mobilDemo2{
+
+}
+class Mobil2{
+    boolean mesinHidup = false;
+    int gigi = 0;
+    public void hidupkanMobil(){
+        mesinHidup = true;
+        System.out.println("Mobil dihidupkan");
+    }
+    void matikanMobil(){
+        mesinHidup = false;
+        System.out.println("Mobil dimatikan");
+    }
+
+    
+}
