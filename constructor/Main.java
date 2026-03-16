5212e5a3b1002c86224b7d6d0d0487417e46f622
wilma <mornlitoralis@gmail.com>
2026-03-16 20:04:52 +0700

    add constructor

---
+++ constructor/Main.java
@@ -0,0 +1,17 @@
+package constructor;
+
+class Siswa{
+    String nama;
+    String kelas;
+    Siswa(){
+        nama = "Belum diisi";
+        kelas = "Belum ada";
+    }
+}
+public class Main{
+    public static void main (String[] args){
+        Siswa s1 = new Siswa();
+        System.out.println("Nama:" + s1.nama);
+        System.out.println("Kelas:" + s1.kelas);
+    }
+}
