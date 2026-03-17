c590ae7e987dd5a3564582253b2f6095f78e1a6e
wilma <mornlitoralis@gmail.com>
2026-03-17 21:38:43 +0700

    finishing encaptulation

--- encaptulation/MahasiswaDemo.java
+++ encaptulation/MahasiswaDemo.java
@@ -3,9 +3,11 @@ package encaptulation;
 class Mahasiswa{
     private int nim;
     private String nama;
-    public Mahasiswa(int nim, String nama){
+    private float IPK;
+    public Mahasiswa(int nim, String nama, float IPK){
         this.nim = nim;
         this.nama = nama;
+        this.IPK = IPK;
     }
     public int getNim(){
         return nim;
@@ -13,13 +15,17 @@ class Mahasiswa{
     public String getNama(){
         return nama;
     }
+    public float getIPK(){
+        return IPK;
+    }
 }
 
 public class MahasiswaDemo{
     public static void main(String[] args){
-        Mahasiswa mhs=new Mahasiswa(11, "Wilma"); 
+        Mahasiswa mhs= new Mahasiswa(11, "Wilma", 3.73f); 
         System.out.println("Nim :" + mhs.getNim());
         System.out.println("Nama :" + mhs.getNama());
+        System.out.println("IPK: " + mhs.getIPK());
     }
 }
 
