7ef4ca652efd170f6262e847abf7a07c9bf6e074
wilma <mornlitoralis@gmail.com>
2026-04-06 10:12:00 +0700

    some changes

--- latihanLab/main.java
+++ latihanLab/main.java
@@ -1,11 +1,44 @@
 class nasabah{
-	String norek; //Attribute
-	String nama;
-	int saldo;
+	private String norek; //Attribute
+	static String nama;
+	private int saldo;
+	static String alamat;
 	public nasabah (String norek, String nama, int saldo){ //konstruktor
 		this.norek = norek;
 		this.nama = nama;
 		this.saldo = saldo;
+	}
+
+	public nasabah(String norek){ //overloading
+		this.norek = norek;
+	}
+	public nasabah (String nama, String alamat){
+		this.nama = nama;
+		this.alamat= alamat;
+	}
+	public nasabah (int saldo){
+		
+	}
+	public void setNorek(String norek){
+		this.norek = norek;
+	}
+
+	String getNorek(){
+		return norek;
+	}
+
+	public void setSaldo(int saldo){
+		this.saldo=saldo;
+	}
+
+	int getSaldo (){
+		return saldo;
+	}
+	String getNama(){
+		return nama;
+	}
+	String getAlamat(){
+		return nama;
 	}
 	void tampil(){
 		System.out.println("No. Rekening	: " + norek);
@@ -16,7 +49,15 @@ class nasabah{
 
 class nasabahBeraksi{
 	public static void main(String[] args){
-		nasabah nas = new nasabah("REK001", "Sunaryo", 100000000);
-		nas.tampil();
+		// nasabah nas = new nasabah("REK001", "Sunaryo", 100000000);
+		nasabah nas2 = new nasabah("REK003");
+		nasabah nas4 = new nasabah("Wilma", "Bima 2");
+		nasabah nas3 = new nasabah (31111);
+		nas3.setSaldo(70000);		// nas.setNorek("REK002");
+		// nas.setSaldo(220000);
+		System.out.println("No. Rekening	:" + nas2.getNorek());
+		System.out.println("Nama		:" + nas4.getNama());
+		System.out.println("Alamat		:" + nas4.getAlamat());
+		System.out.println("Soldo		:" + nas3.getSaldo());
 	}
 }
