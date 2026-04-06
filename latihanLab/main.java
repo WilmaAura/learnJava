523160e6e58359d7a4be1896b6dbb22e60144c0b
wilma <mornlitoralis@gmail.com>
2026-04-06 08:56:20 +0700

    add latihanLab

---
+++ latihanLab/main.java
@@ -0,0 +1,11 @@
+class nasabah{
+	String norek; //Attribute
+	String nama;
+	int saldo;
+	public nasabah (String norek, String nama, int saldo){ //konstruktor
+		this.norek = norek;
+		this.nama = nama;
+		this.saldo = saldo;
+	}
+	
+	}
