ee42a942a9359d78f11ce7c8c48ac3ff978715a9
wilma <mornlitoralis@gmail.com>
2026-04-26 15:49:49 +0700

    add package but not done yet

---
+++ latihanUTS/Package.java
@@ -0,0 +1,15 @@
+package latihanUTS;
+import java.util.Scanner;
+public class Package {
+    
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter Username");
+
+        String userName = obj.nextLine();
+        System.out.println("Username is: " + userName);
+    }
+}
