ddd11a80f4216774783d12614407b9f280f38e40
wilma <mornlitoralis@gmail.com>
2026-04-26 14:12:04 +0700

    add payment to explain abstract implementation

---
+++ latihanUTS/payment.java
@@ -0,0 +1,44 @@
+package latihanUTS;
+
+// Abstract class: The general "blueprint"
+abstract class paymentAbstract {
+    double amount;
+    paymentAbstract(double amount){
+        this.amount = amount;
+    }
+    void printReceipt(){
+        System.out.println("Payment processed: Rp" + amount);
+    }
+
+    //We know every payment must process, but we don't know how yet.
+    abstract void processPayment();
+}
+
+class QRISPayment extends paymentAbstract{
+    QRISPayment(double amount){
+        super(amount);
+    }
+    @Override
+    void processPayment(){
+        System.out.println("generating QR Code for Rp " + amount);
+        System.out.println("Scanning... Success!");
+    }
+}
+
+class BankTransfer extends paymentAbstract {
+    BankTransfer(double amount){
+        super(amount);
+    }
+    void processPayment(){
+        System.out.println("Creating Virtual Account number...");
+        System.out.println("Transfer received via Bank Central!");
+    }
+}
+
+public class payment{
+    public static void main(String[] args) {
+    paymentAbstract qris = new QRISPayment(50000);
+    qris.processPayment();
+    qris.printReceipt();
+    }
+}
