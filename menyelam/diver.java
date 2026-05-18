7000c9d29ee6781633e2f2bf62ff508f6e059f63
wilma <mornlitoralis@gmail.com>
2026-05-18 14:14:39 +0700

    add the diver fiture

--- menyelam/diver.java
+++ menyelam/diver.java
@@ -1,5 +1,38 @@
 package menyelam;
 
+// Memasukkan data profil awal
 public class diver {
-    
+    private String name;
+    private double tankCapacity; //Kapasitas tabung dalam Liter
+    private double tekananAwal; //BAR
+    private double sacRate; //SAC RATE
+
+    public diver(String name, double tankCapacity, double tekananAwal, double sacRate ){
+        this.name = name;
+        this.tankCapacity= tankCapacity;
+        this.tekananAwal = tekananAwal;
+        this.sacRate = sacRate;
+    }
+
+    //Setter dan getter
+    public String getName(){
+        return name;
+    }
+    public double getTankCapacity(){
+        return tankCapacity;
+    }
+    public double getTekananAwal(){
+        return tekananAwal;
+    }public double getSacRate(){
+        return sacRate;
+    }
+
+    // 
+    public void setTekananAwal(double pressure){
+        this.tekananAwal = pressure;
+    }
+
+    public double getAirVolume(){
+        return this.tankCapacity * this.tekananAwal;
+    }
 }
