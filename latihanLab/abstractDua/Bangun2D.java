d49d17b08f0b18cf150c6f7848b88fbe3621f2ca
wilma <mornlitoralis@gmail.com>
2026-05-25 09:46:35 +0700

    add lat abstract

---
+++ latihanLab/abstractDua/Bangun2D.java
@@ -0,0 +1,8 @@
+package asbtractDua;
+
+ 
+
+public abstract class Bangun2D {
+    public abstract void cetakLuas();
+    public abstract void cetakKeliling();
+}
---
+++ latihanLab/abstractDua/Bangun2DDemo.java
@@ -0,0 +1,9 @@
+package asbtractDua;
+
+public class Bangun2DDemo {
+    public static void main(String[] args) {
+        BujurSangkar bs =new BujurSangkar(5);
+        bs.cetakKeliling();
+        bs.cetakLuas();
+    }
+}
