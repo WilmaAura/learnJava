7e3478c461e8b530f0eea3a58c01d9ee4d38f5ba
wilma <mornlitoralis@gmail.com>
2026-05-25 09:01:39 +0700

    first commit

---
+++ latihanLab/Latabstract/Main.java
@@ -0,0 +1,11 @@
+package Latabstract;
+
+abstract class makhlukHidup{
+    public void makan(){
+
+    }
+    public abstract void berjalan(); //aturan yang diterapkan pada subclassnya
+}
+public class Main {
+    
+}
